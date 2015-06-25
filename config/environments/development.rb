7ca41948e74d2866c0b7eb8636bb66018c70d173
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-06-25 17:54:23 +0300

    fix conf mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
+    config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :domain               => "gmail.com",
+        :user_name            => "buhtoyarov1986@gmail.com",
+        :password             => "!!Bukvaz12086",
+        :authentication       => :plain,
+        :enable_starttls_auto => true
+  }
+
 end
