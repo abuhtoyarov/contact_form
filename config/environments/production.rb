f16f5b8af1500981e6dfc7ca33cb524e81c3f896
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-06-25 18:27:46 +0300

    config mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ Rails.application.configure do
         :address              => "smtp.gmail.com",
         :port                 => 587,
         :domain               => "gmail.com",
-        :user_name            => "buhtoyarov1986@gmail.com",
-        :password             => "!!Bukvaz12086",
+        :user_name            => ENV["USERNAME_GMAIL"],
+        :password             => ENV["PASSWORD_GMAIL"],
         :authentication       => :plain,
         :enable_starttls_auto => true
   }
