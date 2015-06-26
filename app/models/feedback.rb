bc8bf1504a0d99392f0df3fb8a0daa1466e419a5
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-06-26 13:12:09 +0300

    fix tabs

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,23 +1,23 @@
 class Feedback < ActiveRecord::Base
 
-	include Humanizer
-	attr_accessor :bypass_humanizer
-	require_human_on :create, :unless => :bypass_humanizer
+  include Humanizer
+  attr_accessor :bypass_humanizer
+  require_human_on :create, :unless => :bypass_humanizer
 
-	validates :name, :email, :message,  presence: true
-	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :name, :email, :message,  presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
-	mount_uploader :file, ImageUploader
+  mount_uploader :file, ImageUploader
 
-	after_create :send_feedback
+  after_create :send_feedback
 
-	def send_mail(message)
-		SendMailJob.perform_later(self.name, self.email, message)
-	end
+  def send_mail(message)
+  	SendMailJob.perform_later(self.name, self.email, message)
+  end
 
-	private
+  private
 
-	def send_feedback
-		SendFeedbackJob.perform_later self
-	end
+  def send_feedback
+  	SendFeedbackJob.perform_later self
+  end
 end
