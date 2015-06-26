95051df7bd177d54ee3b28c25a0d7a32f2ab1651
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-06-26 13:09:37 +0300

    fix tabs

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,44 +1,44 @@
 class FeedbacksController < ApplicationController
 
-	before_action :load_feedback, only: [:show, :destroy, :send_mail]
+  before_action :load_feedback, only: [:show, :destroy, :send_mail]
 
-	respond_to :html, :js
+  respond_to :html, :js
 
-	authorize_resource
+  authorize_resource
 
-	def index
-		respond_with(@feedback = Feedback.all)
-	end
+  def index
+    respond_with(@feedback = Feedback.all)
+  end
 
-	def new
-		respond_with(@feedback = Feedback.new)
-	end
+  def new
+    respond_with(@feedback = Feedback.new)
+  end
 
-	def create
-		respond_with(@feedback = Feedback.create(feedback_params), :location => root_path)
-	end
+  def create
+    respond_with(@feedback = Feedback.create(feedback_params), :location => root_path)
+  end
 
-	def show
-		respond_with @feedback
-	end
+  def show
+    respond_with @feedback
+  end
 
-	def destroy
-		respond_with(@feedback.destroy)
-	end
+  def destroy
+    respond_with(@feedback.destroy)
+  end
 
-	def send_mail
-		@feedback.send_mail(params['message'])
-		redirect_to root_path, notice: "Send ok"
-	end
+  def send_mail
+    @feedback.send_mail(params['message'])
+    redirect_to root_path, notice: "Send ok"
+  end
 
-	private
+  private
 
-	def feedback_params
-		params.require(:feedback).permit(:name, :email, :message, :file, :humanizer_answer, :humanizer_question_id)
-	end
+  def feedback_params
+    params.require(:feedback).permit(:name, :email, :message, :file, :humanizer_answer, :humanizer_question_id)
+  end
 
-	def load_feedback
-		@feedback = Feedback.find(params[:id])
-	end
+  def load_feedback
+    @feedback = Feedback.find(params[:id])
+  end
 
 end
